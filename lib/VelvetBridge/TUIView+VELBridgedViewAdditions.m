f720fc557761acee48187abb7f7df84b0480e256
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-11 16:15:43 -0800

    TUIView+VELBridgedViewAdditions interface

---
+++ lib/VelvetBridge/TUIView+VELBridgedViewAdditions.m
@@ -0,0 +1,13 @@
+//
+//  TUIView+VELBridgedViewAdditions.m
+//  TwUI
+//
+//  Created by Justin Spahr-Summers on 11.01.12.
+//  Copyright (c) 2012 Bitswift. All rights reserved.
+//
+
+#import "TUIView+VELBridgedViewAdditions.h"
+#import <Proton/Proton.h>
+
+@safecategory (TUIView, VELBridgedViewAdditions)
+@end
