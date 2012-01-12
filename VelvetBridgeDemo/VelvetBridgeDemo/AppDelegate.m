00780868754650da30137ef108e3197fd9005ca3
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-01-12 01:26:55 -0800

    Test back-and-forth Velvet/AppKit/TwUI bridging in the VelvetBridgeDemo

--- VelvetBridgeDemo/VelvetBridgeDemo/AppDelegate.m
+++ VelvetBridgeDemo/VelvetBridgeDemo/AppDelegate.m
@@ -13,14 +13,34 @@
 @synthesize window = _window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    VELTUIView *twuiHostView = [[VELTUIView alloc] initWithFrame:CGRectMake(50, 50, 400, 400)];
+    twuiHostView.TUIView = [[TUIView alloc] initWithFrame:twuiHostView.bounds];
+
     TUITextView *textView = [[TUITextView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
     textView.placeholder = @"Type some text?";
     textView.font = [TUIFont systemFontOfSize:17];
+    [twuiHostView.TUIView addSubview:textView];
 
-    VELTUIView *twuiHostView = [[VELTUIView alloc] initWithFrame:CGRectMake(50, 50, 200, 200)];
-    twuiHostView.TUIView = textView;
+    TUIVelvetView *velvetHostView = [[TUIVelvetView alloc] initWithFrame:CGRectMake(250, 100, 100, 100)];
+    velvetHostView.backgroundColor = [TUIColor blueColor];
+    [twuiHostView.TUIView addSubview:velvetHostView];
+
+    NSButton *button = [[NSButton alloc] initWithFrame:NSMakeRect(0, 0, 80, 28)];
+    [button setButtonType:NSMomentaryPushInButton];
+    [button setBezelStyle:NSRoundedBezelStyle];
+    [button setTitle:@"Test Button"];
+    [button setTarget:self];
+    [button setAction:@selector(testButtonPushed:)];
+    
+    VELNSView *buttonHostView = [[VELNSView alloc] initWithNSView:button];
+    buttonHostView.backgroundColor = [NSColor redColor];
+    [velvetHostView.rootView addSubview:buttonHostView];
 
     [self.window.rootView addSubview:twuiHostView];
+}
+
+- (void)testButtonPushed:(id)sender {
+    NSLog(@"%s: %@", __func__, sender);
 }
 
 @end
